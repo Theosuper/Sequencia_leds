79c081425114be3f5f85beb3bd2b7fd94475caaf
Theosuper <theo.graczyk@gmail.com>
2026-01-17 11:34:45 -0300

    desafios de sequencia dos leds

---
+++ sequencia_leds.ino
@@ -0,0 +1,31 @@
+void setup() {
+ pinMode(1,OUTPUT);
+ pinMode(2,OUTPUT);
+ pinMode(3,OUTPUT);
+ pinMode(4,OUTPUT);
+ pinMode(5,OUTPUT);
+}
+
+void loop() {
+  // ligar todos
+ digitalWrite(1,HIGH);
+ delay(100);
+ digitalWrite(1,LOW);
+ delay(200);
+ digitalWrite(2,HIGH);
+ delay(100);
+ digitalWrite(2,LOW);
+ delay(200);
+ digitalWrite(3,HIGH);
+ delay(100);
+ digitalWrite(3,LOW);
+ delay(200);
+ digitalWrite(4,HIGH);
+ delay(100);
+ digitalWrite(4,LOW);
+ delay(200);
+ digitalWrite(5,HIGH);
+ delay(100);
+ digitalWrite(5,LOW);
+ delay(200);
+}
